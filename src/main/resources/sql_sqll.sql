98272e84c52881893d34ff90200e61ab0bfd0591
czhyf <up_boy@aliyun.com>
2018-12-31 12:52:25 +0800

    the first

---
+++ src/main/resources/sql_sqll.sql
@@ -0,0 +1,63 @@
+create database sell
+
+#其他
+create table Other(
+   count1 int(100),
+   count2 int(100),
+   count3 int(100),
+   count4 int(100),
+   count5 int(100),
+   count6 int(100),
+   count7 int(100)
+);
+
+#唯品会
+create table WP(
+   count1 int(100),
+   count2 int(100),
+   count3 int(100),
+   count4 int(100),
+   count5 int(100),
+   count6 int(100),
+   count7 int(100)
+);
+
+#京东
+create table JD(
+   count1 int(100),
+   count2 int(100),
+   count3 int(100),
+   count4 int(100),
+   count5 int(100),
+   count6 int(100),
+   count7 int(100)
+);
+
+#淘宝
+create table TB(
+   count1 int(100),
+   count2 int(100),
+   count3 int(100),
+   count4 int(100),
+   count5 int(100),
+   count6 int(100),
+   count7 int(100)
+);
+
+#天猫
+create table TM(
+   count1 int(100),
+   count2 int(100),
+   count3 int(100),
+   count4 int(100),
+   count5 int(100),
+   count6 int(100),
+   count7 int(100)
+);
+
+
+初始化值全部为0
+
+
+
+
